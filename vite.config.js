75f41427f1d1ab596e615a79b9bbd2f986873c9b
Charles Severance <csev@umich.edu>
2025-03-14 14:59:22 -0400

    MVP Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [{
+    name: 'remove-importmap',
+    transformIndexHtml(html) {
+      // Remove the importmap script tag
+      return html.replace(
+        /<script type="importmap">[\s\S]*?<\/script>/i,
+        ''
+      );
+    }
+  }],
   build: {
     // Will read your import maps and bundle accordingly
     modulePreload: true,
